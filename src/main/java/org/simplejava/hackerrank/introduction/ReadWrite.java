0cab9e75e385d5b1c04357d8c2ba9e82ff2f2fcc
Brandon <brandon.sum@gmail.com>
2016-02-21 23:31:44 -0500

    added new files

---
+++ src/main/java/org/simplejava/hackerrank/introduction/ReadWrite.java
@@ -0,0 +1,20 @@
+package org.simplejava.hackerrank.introduction;
+
+import java.util.Scanner;
+
+public class ReadWrite {
+
+	public static void main(String[] args) {
+	      Scanner sc=new Scanner(System.in);
+	      int a=sc.nextInt();
+	      int b=sc.nextInt();
+	      int c=sc.nextInt();
+
+	      System.out.println(a);
+	      System.out.println(b);
+	      System.out.println(c);
+	    
+	      sc.close();
+	}
+
+}
